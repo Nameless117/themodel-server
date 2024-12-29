e78d594b879afe0163d479eb3d00d0a6b26bbc33
o1was <guanlinhai520@gmail.com>
2024-12-29 17:06:40 +0800

    ✨ Feat(logo addr): logo addr
    logo addr

--- app/site/models/sys_site_config.go
+++ app/site/models/sys_site_config.go
@@ -1,24 +1,23 @@
 package models
 
 import (
-
 	"go-admin/common/models"
-
 )
 
 type SysSiteConfig struct {
-    models.Model
-    
-    LogoLarge string `json:"logoLarge" gorm:"type:varchar(255);comment:左上角大logo URL"` 
-    LogoSmall string `json:"logoSmall" gorm:"type:varchar(255);comment:右上角小logo URL"` 
-    BannerBgImg string `json:"bannerBgImg" gorm:"type:varchar(255);comment:背景图片 URL"` 
-    BannerBook string `json:"bannerBook" gorm:"type:varchar(255);comment:书封面图片 URL"` 
-    models.ModelTime
-    models.ControlBy
+	models.Model
+
+	LogoLarge     string `json:"logoLarge" gorm:"type:varchar(255);comment:左上角大logo URL"`
+	LogoSmall     string `json:"logoSmall" gorm:"type:varchar(255);comment:右上角小logo URL"`
+	LogoSmallAddr string `json:"logoSmallAddr" gorm:"type:varchar(255);comment:右上角小logo 跳转地址URL"`
+	BannerBgImg   string `json:"bannerBgImg" gorm:"type:varchar(255);comment:背景图片 URL"`
+	BannerBook    string `json:"bannerBook" gorm:"type:varchar(255);comment:书封面图片 URL"`
+	models.ModelTime
+	models.ControlBy
 }
 
 func (SysSiteConfig) TableName() string {
-    return "sys_site_config"
+	return "sys_site_config"
 }
 
 func (e *SysSiteConfig) Generate() models.ActiveRecord {
--- app/site/service/dto/sys_site_config.go
+++ app/site/service/dto/sys_site_config.go
@@ -1,26 +1,24 @@
 package dto
 
 import (
-
 	"go-admin/app/site/models"
 	"go-admin/common/dto"
 	common "go-admin/common/models"
 )
 
 type SysSiteConfigGetPageReq struct {
-	dto.Pagination     `search:"-"`
-    SysSiteConfigOrder
+	dto.Pagination `search:"-"`
+	SysSiteConfigOrder
 }
 
 type SysSiteConfigOrder struct {
-    Id string `form:"idOrder"  search:"type:order;column:id;table:sys_site_config"`
-    LogoLarge string `form:"logoLargeOrder"  search:"type:order;column:logo_large;table:sys_site_config"`
-    LogoSmall string `form:"logoSmallOrder"  search:"type:order;column:logo_small;table:sys_site_config"`
-    BannerBgImg string `form:"bannerBgImgOrder"  search:"type:order;column:banner_bg_img;table:sys_site_config"`
-    BannerBook string `form:"bannerBookOrder"  search:"type:order;column:banner_book;table:sys_site_config"`
-    CreatedAt string `form:"createdAtOrder"  search:"type:order;column:created_at;table:sys_site_config"`
-    UpdatedAt string `form:"updatedAtOrder"  search:"type:order;column:updated_at;table:sys_site_config"`
-    
+	Id          string `form:"idOrder"  search:"type:order;column:id;table:sys_site_config"`
+	LogoLarge   string `form:"logoLargeOrder"  search:"type:order;column:logo_large;table:sys_site_config"`
+	LogoSmall   string `form:"logoSmallOrder"  search:"type:order;column:logo_small;table:sys_site_config"`
+	BannerBgImg string `form:"bannerBgImgOrder"  search:"type:order;column:banner_bg_img;table:sys_site_config"`
+	BannerBook  string `form:"bannerBookOrder"  search:"type:order;column:banner_book;table:sys_site_config"`
+	CreatedAt   string `form:"createdAtOrder"  search:"type:order;column:created_at;table:sys_site_config"`
+	UpdatedAt   string `form:"updatedAtOrder"  search:"type:order;column:updated_at;table:sys_site_config"`
 }
 
 func (m *SysSiteConfigGetPageReq) GetNeedSearch() interface{} {
@@ -28,22 +26,24 @@ func (m *SysSiteConfigGetPageReq) GetNeedSearch() interface{} {
 }
 
 type SysSiteConfigInsertReq struct {
-    Id int `json:"-" comment:""` // 
-    LogoLarge string `json:"logoLarge" comment:"左上角大logo URL"`
-    LogoSmall string `json:"logoSmall" comment:"右上角小logo URL"`
-    BannerBgImg string `json:"bannerBgImg" comment:"背景图片 URL"`
-    BannerBook string `json:"bannerBook" comment:"书封面图片 URL"`
-    common.ControlBy
+	Id            int    `json:"-" comment:""` //
+	LogoLarge     string `json:"logoLarge" comment:"左上角大logo URL"`
+	LogoSmall     string `json:"logoSmall" comment:"右上角小logo URL"`
+	LogoSmallAddr string `json:"logoSmallAddr" comment:"右上角小logo 跳转地址URL"`
+	BannerBgImg   string `json:"bannerBgImg" comment:"背景图片 URL"`
+	BannerBook    string `json:"bannerBook" comment:"书封面图片 URL"`
+	common.ControlBy
 }
 
-func (s *SysSiteConfigInsertReq) Generate(model *models.SysSiteConfig)  {
-    if s.Id == 0 {
-        model.Model = common.Model{ Id: s.Id }
-    }
-    model.LogoLarge = s.LogoLarge
-    model.LogoSmall = s.LogoSmall
-    model.BannerBgImg = s.BannerBgImg
-    model.BannerBook = s.BannerBook
+func (s *SysSiteConfigInsertReq) Generate(model *models.SysSiteConfig) {
+	if s.Id == 0 {
+		model.Model = common.Model{Id: s.Id}
+	}
+	model.LogoLarge = s.LogoLarge
+	model.LogoSmall = s.LogoSmall
+	model.LogoSmallAddr = s.LogoSmallAddr
+	model.BannerBgImg = s.BannerBgImg
+	model.BannerBook = s.BannerBook
 }
 
 func (s *SysSiteConfigInsertReq) GetId() interface{} {
@@ -51,22 +51,24 @@ func (s *SysSiteConfigInsertReq) GetId() interface{} {
 }
 
 type SysSiteConfigUpdateReq struct {
-    Id int `uri:"id" comment:""` // 
-    LogoLarge string `json:"logoLarge" comment:"左上角大logo URL"`
-    LogoSmall string `json:"logoSmall" comment:"右上角小logo URL"`
-    BannerBgImg string `json:"bannerBgImg" comment:"背景图片 URL"`
-    BannerBook string `json:"bannerBook" comment:"书封面图片 URL"`
-    common.ControlBy
+	Id            int    `uri:"id" comment:""` //
+	LogoLarge     string `json:"logoLarge" comment:"左上角大logo URL"`
+	LogoSmall     string `json:"logoSmall" comment:"右上角小logo URL"`
+	LogoSmallAddr string `json:"logoSmallAddr" comment:"右上角小logo 跳转地址URL"`
+	BannerBgImg   string `json:"bannerBgImg" comment:"背景图片 URL"`
+	BannerBook    string `json:"bannerBook" comment:"书封面图片 URL"`
+	common.ControlBy
 }
 
-func (s *SysSiteConfigUpdateReq) Generate(model *models.SysSiteConfig)  {
-    if s.Id == 0 {
-        model.Model = common.Model{ Id: s.Id }
-    }
-    model.LogoLarge = s.LogoLarge
-    model.LogoSmall = s.LogoSmall
-    model.BannerBgImg = s.BannerBgImg
-    model.BannerBook = s.BannerBook
+func (s *SysSiteConfigUpdateReq) Generate(model *models.SysSiteConfig) {
+	if s.Id == 0 {
+		model.Model = common.Model{Id: s.Id}
+	}
+	model.LogoLarge = s.LogoLarge
+	model.LogoSmall = s.LogoSmall
+	model.LogoSmallAddr = s.LogoSmallAddr
+	model.BannerBgImg = s.BannerBgImg
+	model.BannerBook = s.BannerBook
 }
 
 func (s *SysSiteConfigUpdateReq) GetId() interface{} {
@@ -75,8 +77,9 @@ func (s *SysSiteConfigUpdateReq) GetId() interface{} {
 
 // SysSiteConfigGetReq 功能获取请求参数
 type SysSiteConfigGetReq struct {
-     Id int `uri:"id"`
+	Id int `uri:"id"`
 }
+
 func (s *SysSiteConfigGetReq) GetId() interface{} {
 	return s.Id
 }
